e793d280a99ea4e02d515b7164a76c34a2440290
lmifflen <lmifflen@gmail.com>
2022-04-12 22:20:07 -0600

    fixed query - need to now parse information from query i want

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -4,6 +4,9 @@ import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
+// const layer = { 
+//   name: "points",
+
 const MapView = () => {
   const [lng, setLng] = useState(-114.07);
   const [lat, setLat] = useState(51.05);
@@ -26,41 +29,55 @@ const MapView = () => {
       setZoom(map.getZoom().toFixed(2));
     });
 
-    map.on('load', () => {
-        map.addSource('park-pathways', {
-            type: 'vector',
-            url: 'mapbox://miffll.park-pathways'
-        });
-        map.addLayer({
-            'id': 'GreenwayNorth',
-            'type': 'line',
-            'source': 'park-pathways',
-            'source-layer': 'GreenwayNorth',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': 'rgb(0, 255, 0)',
-                'line-width': 10
-            }
-        });
-        
-     map.on('click', (e) => {
+    map.on('click', (e) => {
 
-        const bbox = [
-            [e.point.x - 6, e.point.y - 6],
-            [e.point.x + 6, e.point.y + 6]
-        ];
+      const bbox = [
+          [e.point.x - 6, e.point.y - 6],
+          [e.point.x + 6, e.point.y + 6]
+      ];
+      let x = map.queryRenderedFeatures(bbox);
+      setFeature(x);
+      console.log(x);
+    });
 
-        setFeature(map.queryRenderedFeatures(bbox, {layers: [map.getSource('composite').vectorLayerIds]}));
+    // map.on('load', () => {
+    //     map.addSource('park-pathways', {
+    //         type: 'vector',
+    //         url: 'mapbox://miffll.park-pathways'
+    //     });
+    //     map.addLayer({
+    //         'id': 'GreenwayNorth',
+    //         'type': 'line',
+    //         'source': 'park-pathways',
+    //         'source-layer': 'GreenwayNorth',
+    //         'layout': {
+    //             'line-join': 'round',
+    //             'line-cap': 'round'
+    //         },
+    //         'paint': {
+    //             'line-color': 'rgb(0, 255, 0)',
+    //             'line-width': 10
+    //         }
+    //     });
+        
+    //  map.on('click', (e) => {
+
+    //     const bbox = [
+    //         [e.point.x - 6, e.point.y - 6],
+    //         [e.point.x + 6, e.point.y + 6]
+    //     ];
+    //     let x = map.queryRenderedFeatures(bbox, {layers: ['GreenwayNorth', 'GreenwaySouth',]});
+    //     setFeature(x);
+    //     console.log(x);
+
+    //     // setFeature(map.queryRenderedFeatures(bbox, {layers: [map.getSource('composite').vectorLayerIds]}));
         
         
-    });
-    console.log(map.getSource('composite').vectorLayerIds)
-    console.log(feature);
-    });
-  }, [feature]);
+    // });
+    // console.log(map.getSource('composite').vectorLayerIds)
+  
+    // });
+  }, []);
 
   return (
     <div className="map">
