4efd304203586538c6b6d1c7df43995f8e476542
lmifflen <lmifflen@gmail.com>
2022-04-18 08:52:55 -0600

    cleaning up map file

--- client/src/components/map/Map.jsx
+++ client/src/components/map/Map.jsx
@@ -29,9 +29,7 @@ const MapView = () => {
       positionOptions: {
       enableHighAccuracy: true
       },
-      // When active the map will receive updates to the device's location as it changes.
       trackUserLocation: true,
-      // Draw an arrow next to the location dot to indicate which direction the device is heading.
       showUserHeading: true
       })
     );
