528904071e87f85da06cb172b694ed7e3a09b8d2
Sanja <sivansic@gmail.com>
2022-04-20 19:19:10 -0600

    Changes to comments

--- client/src/components/comments/Comment.js
+++ client/src/components/comments/Comment.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./Comments.css"
 
-const Comment = () => {
+
+const Comment = ({comment}) => {
   return (
-    <div>Hello Comment</div>
+    <div className='comment'>
+      <div className='comment-image-container'>
+        Slika
+      </div>
+      <div className='comment-right-part'>
+        <div className='comment-content'>
+          <div className='comment-author'>{comment.name}</div>
+          <div>{comment.createdAt}</div>
+        </div>
+        <div className='comment-text'>{comment.comment}</div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/comments/Comments.js
+++ client/src/components/comments/Comments.js
@@ -1,36 +1,78 @@
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Comment from "./Comment";
+import "./Comments.css"
 
-const Comments = () => {
+const Comments = (_id) => {
   const { isAuthenticated } = useAuth0();
   const { user } = useAuth0();
-  const [backendUsers, setBackendUsers] = useState();
+  const [backendUsers, setBackendUsers] = useState([]);
+  const [backendComments, setBackendComments] = useState([]);
+  const rootComments = backendComments.filter(
+    (backendComment) => backendComment.parentId === null
+  );
 
-
-  const finduseremail = async () => {
+  const allcomments = async () => {
     try {
-      if (user == null) setBackendUsers(null)
-      let email = user.email
-      let response = await fetch(`/findusersbyemail?email=${email}`);
-      let allusers = await response.json();
-          return setBackendUsers(allusers);
+      // if (user == null) setBackendUsers(null)
+      // let email = user.email
+      let response = await fetch("/allcomments");
+      let allcomments = await response.json();
+      return setBackendComments(allcomments);
     } catch (ex) {
       console.log(ex);
     }
   };
 
   useEffect(() => {
-    finduseremail();
-  }, [isAuthenticated]);
+    allcomments();
+  }, []);
+
+  // const finduseremail = async () => {
+  //   try {
+  //     if (user == null) setBackendUsers(null)
+  //     let email = user.email
+  //     let response = await fetch(`/findusersbyemail?email=${email}`);
+  //     let allusers = await response.json();
+  //         return setBackendUsers(allusers);
+  //   } catch (ex) {
+  //     console.log(ex);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   finduseremail();
+  // }, [isAuthenticated]);
 
   return (
     <div className="comments">
       <h3 className="comments-title">Comments</h3>
       <div className="comment-form-title">Write comment</div>
-      <div>
+
+      <div className="comments-container">
+        {/* {rootComments.map((rootComment) => (
+          <div key={rootComment._id}>{rootComment.body}</div>
+        ))} */}
+        {/* {backendComments.map((backcomment) => (
+          <p>
+            {backcomment.name} {backcomment.comment} {backcomment.createdAt}
+          </p>
+        ))} */}
+        {/* {backendComments.map((backComment) => ( */}
+        {rootComments.map((rootComment) => (
+          // <div key={backcomment._id}>{backcomment.body}</div>
+          <Comment
+           key={rootComment._id}
+           comment={rootComment}
+           />
+        ))}
+      </div>
+      
+      
+      {/* <div>
         {isAuthenticated && backendUsers != null &&
           backendUsers.map((backuser) => <p>{backuser.email}</p>)}
-      </div>
+      </div> */}
     </div>
   );
 };
