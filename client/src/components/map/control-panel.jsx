45423e56bde699aeb81efa47d0608310161307e5
lmifflen <lmifflen@gmail.com>
2022-04-20 19:16:28 -0600

    added control panel

--- client/src/components/map/control-panel.jsx
+++ client/src/components/map/control-panel.jsx
@@ -1,13 +1,19 @@
-import * as React from 'react';
-import {useState, useEffect} from 'react';
-import {fromJS} from 'immutable';
-import MAP_STYLE from './style.json';
-
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { fromJS } from "immutable";
+import MAP_STYLE from "./style.json";
 
 const defaultMapStyle = fromJS(MAP_STYLE);
-const defaultLayers = defaultMapStyle.get('layers');
+const defaultLayers = defaultMapStyle.get("layers");
 
-const categories = ['labels', 'roads', 'buildings', 'parks', 'water', 'background'];
+const categories = [
+  "labels",
+  "roads",
+  "buildings",
+  "parks",
+  "water",
+  "background",
+];
 
 // Layer id patterns by category
 const layerSelector = {
@@ -16,34 +22,36 @@ const layerSelector = {
   parks: /park/,
   buildings: /building/,
   roads: /bridge|road|tunnel/,
-  labels: /label|place|poi/
+  labels: /label|place|poi/,
 };
 
 // Layer color class by type
 const colorClass = {
-  line: 'line-color',
-  fill: 'fill-color',
-  background: 'background-color',
-  symbol: 'text-color'
+  line: "line-color",
+  fill: "fill-color",
+  background: "background-color",
+  symbol: "text-color",
 };
 
-function getMapStyle({visibility, color}) {
+function getMapStyle({ visibility, color }) {
   const layers = defaultLayers
-    .filter(layer => {
-      const id = layer.get('id');
-      return categories.every(name => visibility[name] || !layerSelector[name].test(id));
+    .filter((layer) => {
+      const id = layer.get("id");
+      return categories.every(
+        (name) => visibility[name] || !layerSelector[name].test(id)
+      );
     })
-    .map(layer => {
-      const id = layer.get('id');
-      const type = layer.get('type');
-      const category = categories.find(name => layerSelector[name].test(id));
+    .map((layer) => {
+      const id = layer.get("id");
+      const type = layer.get("type");
+      const category = categories.find((name) => layerSelector[name].test(id));
       if (category && colorClass[type]) {
-        return layer.setIn(['paint', colorClass[type]], color[category]);
+        return layer.setIn(["paint", colorClass[type]], color[category]);
       }
       return layer;
     });
 
-  return defaultMapStyle.set('layers', layers);
+  return defaultMapStyle.set("layers", layers);
 }
 
 function StyleControls(props) {
@@ -53,49 +61,39 @@ function StyleControls(props) {
     buildings: true,
     roads: true,
     labels: true,
-    background: true
+    background: true,
   });
 
   const [color, setColor] = useState({
-    water: '#DBE2E6',
-    parks: '#E6EAE9',
-    buildings: '#c0c0c8',
-    roads: '#ffffff',
-    labels: '#78888a',
-    background: '#EBF0F0'
+    water: "#DBE2E6",
+    parks: "#E6EAE9",
+    buildings: "#c0c0c8",
+    roads: "#ffffff",
+    labels: "#78888a",
+    background: "#EBF0F0",
   });
 
   useEffect(() => {
-    props.onChange(getMapStyle({visibility, color}));
+    props.onChange(getMapStyle({ visibility, color }));
   }, [visibility, color]);
 
-
   const onVisibilityChange = (name, value) => {
-    setVisibility({...visibility, [name]: value});
+    setVisibility({ ...visibility, [name]: value });
   };
 
   return (
     <div className="control-panel">
-      <h3>Dynamic Styling</h3>
-      <p>Dynamically show/hide map layers and change color with Immutable map style.</p>
-      <div className="source-link">
-        <a
-          href="https://github.com/visgl/react-map-gl/tree/7.0-release/examples/layers"
-          target="_new"
-        >
-          View Code ↗
-        </a>
-      </div>
+      <h3>Toggle layers</h3>
+
       <hr />
-      {categories.map(name => (
+      {categories.map((name) => (
         <div key={name} className="input">
           <label>{name}</label>
           <input
             type="checkbox"
             checked={visibility[name]}
-            onChange={evt => onVisibilityChange(name, evt.target.checked)}
+            onChange={(evt) => onVisibilityChange(name, evt.target.checked)}
           />
-          
         </div>
       ))}
     </div>
