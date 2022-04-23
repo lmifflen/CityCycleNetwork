c26e9fb6e69d60bc0f4430fa3eacf52d6301baa4
lmifflen <lmifflen@gmail.com>
2022-04-23 08:18:33 -0600

    have info and get features working properly.

--- client/src/components/info/Info.jsx
+++ client/src/components/info/Info.jsx
@@ -1,22 +1,16 @@
 import info from "./pathwayInfo";
 import "./Info.css";
 import BowCyclepath from "../../images/BowCyclepath.jpg";
-import { useMap } from "react-map-gl";
-import { useState } from "react";
-
 
 const Info = (props) => {
-
-  
-          
-  const { feature }  = props;
+  const { feature } = props;
 
   const matchingKey = Object.keys(info).find((key) => {
     return feature?.some((vector) => {
       return vector.sourceLayer === key;
     });
   });
-  console.log(feature)
+  console.log(feature);
   if (!matchingKey) {
     return (
       <div>
--- client/src/components/map/MapView.jsx
+++ client/src/components/map/MapView.jsx
@@ -1,12 +1,14 @@
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useState } from "react";
+import * as React from "react";
 import "./Map.css";
-import Info from "../info/Info";
 import "mapbox-gl/dist/mapbox-gl.css";
-import Map, { FullscreenControl, GeolocateControl, ScaleControl } from "react-map-gl";
+import Map, {
+  FullscreenControl,
+  GeolocateControl,
+  ScaleControl,
+} from "react-map-gl";
 import ControlPanel from "./control-panel";
-import ControlPanel2 from "./control-panel2";
-import MAP_STYLE from "./style.json";
-
+import GetFeature from "./GetFeature";
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
 
@@ -14,40 +16,7 @@ const MapView = () => {
   const [lng, setLng] = useState(-114.07);
   const [lat, setLat] = useState(51.05);
   const [zoom, setZoom] = useState(12);
-  const [feature, setFeature] = useState(null);
   const [mapStyle, setMapStyle] = useState(null);
-
-  const interactiveLayerIds = ["greenwaynorth"];
-
-  // const onInteractiveLayersChange = useCallback(layerFilter => {
-  //   //   setInteractiveLayerIds(MAP_STYLE.layers.map(layer => layer.id).filter(layerFilter));
-  //   // }, []);
-
-  const onClick = (e) => {
-    console.log(e);
-    setFeature(e);
-  };
-
-
-  
-  // const [cursor, setCursor] = useState('auto');
-  // const [interactiveLayerIds, setInteractiveLayerIds] = useState();
-
-  // const onInteractiveLayersChange = useCallback(layerFilter => {
-  //   setInteractiveLayerIds(MAP_STYLE.layers.map(layer => layer.id).filter(layerFilter));
-  // }, []);
-
-  // const onClick = useCallback(event => {
-  //   const feature = event.features && event.features[0];
-
-  //   if (feature) {
-  //     window.alert(`Clicked layer ${feature.layer.id}`); // eslint-disable-line no-alert
-  //   }
-  // }, []);
-
-  // const onMouseEnter = useCallback(() => setCursor('pointer'), []);
-  // const onMouseLeave = useCallback(() => setCursor('auto'), []);
-
 
   return (
     <div className="map" id="mapbox">
@@ -65,23 +34,16 @@ const MapView = () => {
             }}
             mapStyle={mapStyle && mapStyle.toJS()}
             styleDiffing
-            onClick={onClick}
-            // onMouseEnter={onMouseEnter}
-            // onMouseLeave={onMouseLeave}
-            // cursor={cursor}
-            interactiveLayerIds={interactiveLayerIds}
             mapboxAccessToken={MAPBOX_TOKEN}
           >
             <GeolocateControl />
             <FullscreenControl />
             <ScaleControl />
-
+            <GetFeature />
           </Map>
-          {/* <ControlPanel2 onChange={onInteractiveLayersChange} /> */}
 
           <ControlPanel onChange={setMapStyle} />
         </>
-        <div className="info">{<Info feature={feature}></Info>}</div>
       </div>
     </div>
   );
